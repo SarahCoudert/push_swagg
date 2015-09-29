d7cd2e60a28b2ec41cb46933f0382e8ea8174a04
Sarah COUDERT <scoudert@e1r7p3.42.fr>
2015-09-29 18:08:49 +0200

    quick commit

--- libft/ft_lstcpelement.c
+++ libft/ft_lstcpelement.c
@@ -12,13 +12,6 @@
 
 #include "includes/libft.h"
 
-/*
- ** copy_from is the element to copy
- ** we copy copy_from to the begin of copy_to
- ** element is the new element that we copied from copy_from
- **
-*/
-
 t_list			*ft_lstcpelement(t_list *copy_from, t_list *copy_to)
 {
 	t_list		*element;
